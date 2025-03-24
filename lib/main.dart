5acf15ac1c5b760e9c86e97d7b61ba958f2bc5ff
sajeebro <sajithmadusanka720@gmail.com>
2025-03-24 13:33:23 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:weatherapp/currentWeather.dart';
-
+import 'package:device_preview/device_preview.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      tools: const [
+        ...DevicePreview.defaultTools,
+      ],
+      builder: (context) =>const MyApp())
+);
 }
 
 class MyApp extends StatelessWidget {
